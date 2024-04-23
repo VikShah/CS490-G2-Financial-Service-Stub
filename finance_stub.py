73c2e804c496b687f03e746bdf91e10a41eefb74
Michael Jones <mjj4@njit.edu>
2024-04-23 16:43:26 -0400

    assign credit score finished

--- finance_stub.py
+++ finance_stub.py
@@ -5,26 +5,38 @@ from shapely.geometry import Point
 from datetime import datetime, timedelta, timezone
 from pymysql.err import MySQLError
 import random
+import json
 
 app = Flask(__name__)
-CORS(app) 
+
+#from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, unset_jwt_cookies, jwt_required, JWTManager
+
+app = Flask(__name__)
+CORS(app)
+
+#jwt app token management
+app.config["JWT_SECRET_KEY"] = "SecretKey"
+#jwt = JWTManager(app)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+#end of jwt token management
 
 HOST = "localhost"
 USER = "root"
-PASSWORD = ""
-DB = "CS490"
-
+PASSWORD = "Minecraft928"
+DB = "f_stub"
 
 @app.route("/assignCreditScore", methods=['POST'])
 def assignCreditScore():
     db = pymysql.connect(host=HOST, user=USER, password=PASSWORD, db=DB)
     data = request.get_json()
     #See if user has a credit score
-    user_id = data.get("userID")
+    user_id = data.get("user_id")
+    account_id = "100" + str(user_id)
     with db.cursor() as cursor:
-        query = """SELECT score 
-                    FROM creditScore WHERE user_id = %s"""
-        cursor.execute(query, ([user_id]))
+        query = """SELECT credit_score  
+                    FROM account_info 
+                    WHERE account_id = %s"""
+        cursor.execute(query, ([account_id]))
         results = cursor.fetchall()
         if results:
             return jsonify(results), 200
@@ -32,7 +44,11 @@ def assignCreditScore():
     scores = [757, 763, 785, 660, 701, 584, 642, 619, 563, 555]
     u_credit_score = scores[random.randint(0,9)]
     with db.cursor() as cursor:
-        query = """INSERT INTO creditScore (user_id,score) values (%s,%s)"""
-        cursor.execute(query, (user_id,u_credit_score))
+        query = """INSERT INTO account_info (account_id,credit_score,last_request_date) values (%s,%s,NOW())"""
+        cursor.execute(query, (account_id,u_credit_score))
         db.commit()
     return jsonify(u_credit_score)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5005)
